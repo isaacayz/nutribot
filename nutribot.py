46aebb51967a0eda904bf49c54dbb9a8d727dc9c
AmusanAyodeji <ayobayonleamusan1@gmail.com>
2023-09-08 15:25:18 +0100

    Made some changes

--- nutribot.py
+++ nutribot.py
@@ -9,7 +9,7 @@ key = constant.api_key
 co = cohere.Client(key)
 
 def prompter():
-  prompt = input('What would you like to know? ')
+  prompt = input('Welcome to the nutrition bot. All tasks related to food and nutrition can be answered here. What would you like to ask?')
   response = co.generate(
     prompt=prompt,
     num_generations=1,
@@ -20,29 +20,30 @@ def prompter():
 
 #prompter()
 
-"""The Classification **Endpoint**"""
-examples=[
-  Example("Dermatologists don't like her!", "Spam"),
-  Example("'Hello, open to this?'", "Spam"),
-  Example("I need help please wire me $1000 right now", "Spam"),
-  Example("Nice to know you ;)", "Spam"),
-  Example("Please help me?", "Spam"),
-  Example("Your parcel will be delivered today", "Not spam"),
-  Example("Review changes to our Terms and Conditions", "Not spam"),
-  Example("Weekly sync notes", "Not spam"),
-  Example("'Re: Follow up from today's meeting'", "Not spam"),
-  Example("Pre-read for tomorrow", "Not spam"),
-]
-def classifier():
-  prompts = list(input('Please enter your query '))
-  response = co.classify(
-    inputs=prompts,
-    examples=examples,
-    )
-  return response[0]['labels']
+"""
+  The Classification **Endpoint**
+  examples=[
+    Example("Dermatologists don't like her!", "Spam"),
+    Example("'Hello, open to this?'", "Spam"),
+    Example("I need help please wire me $1000 right now", "Spam"),
+    Example("Nice to know you ;)", "Spam"),
+    Example("Please help me?", "Spam"),
+    Example("Your parcel will be delivered today", "Not spam"),
+    Example("Review changes to our Terms and Conditions", "Not spam"),
+    Example("Weekly sync notes", "Not spam"),
+    Example("'Re: Follow up from today's meeting'", "Not spam"),
+    Example("Pre-read for tomorrow", "Not spam"),
+  ]
+  def classifier():
+    prompts = list(input('Please enter your query '))
+    response = co.classify(
+      inputs=prompts,
+      examples=examples,
+      )
+    return response[0]['labels']
 
 
-"""inputs=[
+inputs=[
   "Confirm your email address",
   "hey i need u to send some $",
 ]
@@ -51,9 +52,9 @@ notspamlabels = []
 for a in response.classifications:
   spamlabels.append(a.labels['Spam'])
   notspamlabels.append(a.labels['Not spam'])
-"""
-#print(classifier())
 
+#print(classifier())
+"""
 def summarize(filepath):
   # Open a file for reading
   try:
@@ -71,4 +72,4 @@ def summarize(filepath):
   except Exception as e:
       print(f"An error occurred: {str(e)}")
 
-print(summarize('/Users/isaacige/Documents/Code/DS&ML/nutribot/story.txt'))
+print(summarize("C:\\Users\\ayoba\Documents\Python Class\\nutribot\story.txt"))
